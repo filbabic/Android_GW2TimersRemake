76b434fb1e5ae339d4b642c6f9b5f083c0280b89
Dominik Babic <domynick93@hotmail.com>
2016-09-22 10:32:07 +0200

    Test

---
+++ app/src/main/java/hr/etfos/d1babic/guildwars2timersremake/MainActivity.java
@@ -0,0 +1,13 @@
+package hr.etfos.d1babic.guildwars2timersremake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
